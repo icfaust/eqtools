828a33a61786a70661d357cd35c2612699b8916a
icfaust <icfaust@gmail.com>
2025-05-06 00:34:16 +0200

    test

--- tests/unittests.py
+++ tests/unittests.py
@@ -30,11 +30,10 @@ except:
         import pickle as pkl
     dir_path = os.path.dirname(os.path.realpath(__file__))    
     with open(os.sep.join([dir_path, 'test_data.pkl']), 'rb') as f:
-        shot, e, et = pkl.load(f, encoding='latin1')
+        shot, e, et = pkl.load(f, encoding='bytes')
         if not eqtools.core._has_trispline:
             et = e
 
-print(dir(e))
 scalar_R = 0.75
 scalar_Z = 0.1
 scalar_t = e.getTimeBase()[45]
