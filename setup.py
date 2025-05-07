8e9d7ea93f2301fd3274ed4e26f3f035a3e2af8d
icfaust <icfaust@gmail.com>
2025-05-07 11:56:41 +0200

    prep for f2py removal:

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from numpy.distutils.core import setup, Extension
+import numpy
+from setuptools import Extension, setup
 
 tricub = Extension(
-    'eqtools._tricub', ['eqtools/_tricub.pyf', 'eqtools/_tricub.c']
+    name='eqtools._tricub', sources=['eqtools/_tricubmodule.c'], include_dirs=[numpy.get_include()]
 )
 
 setup(
